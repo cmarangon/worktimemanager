23dc92d4932e7713b06173b8759ad1ab82619476
Claudio Marangon <marangon.claudio@gmail.com>
2016-09-17 23:29:43 +0200

    Enable client edit

--- src/main/java/ch/hftm/wtm/presentation/ManageClientBean.java
+++ src/main/java/ch/hftm/wtm/presentation/ManageClientBean.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 import ch.hftm.wtm.business.ClientBusiness;
 import ch.hftm.wtm.business.PersonBusiness;
@@ -13,12 +13,14 @@ import ch.hftm.wtm.model.Client;
 import ch.hftm.wtm.model.Person;
 
 @ManagedBean(name = "manageClientBean")
-@RequestScoped
+@SessionScoped
 public class ManageClientBean implements Serializable {
     private static final long serialVersionUID = -3271448299155445895L;
 
     private Client client = new Client();
     private Person administrator = new Person();
+
+    private Client selectedClient;
 
     private List<Client> clients = new ClientBusiness().getClients();
 
@@ -64,15 +66,33 @@ public class ManageClientBean implements Serializable {
         this.clients = clients;
     }
 
+    /**
+     * @return the selectedClient
+     */
+    public Client getSelectedClient() {
+        return selectedClient;
+    }
+
+    /**
+     * @param selectedClient the selectedClient to set
+     */
+    public String setSelectedClient(Client selectedClient) {
+        this.selectedClient = selectedClient;
+
+        return "editClient";
+    }
+
+    /**
+     * @return
+     */
     public String save() {
-        // Starte Transaction
         ClientBusiness cb = new ClientBusiness();
         if (!cb.clientExists(client.getCompanyName())) {
             PersonBusiness pb = new PersonBusiness();
             if (!pb.personExists(administrator.getEmail())) {
 
                 administrator.setAssignedUserRole(UserRole.ADMIN);
-                administrator.setPassword("notsorandomhash");
+                administrator.setPassword("123");
 
                 pb.savePerson(administrator);
             }
@@ -90,6 +110,20 @@ public class ManageClientBean implements Serializable {
         return "overviewClient";
     }
 
+    /**
+     * @return
+     */
+    public String saveSelectedClient() {
+        ClientBusiness cb = new ClientBusiness();
+        cb.saveClient(selectedClient);
+
+        return "overviewClient";
+    }
+
+    /**
+     * @return
+     * @throws Exception
+     */
     public String checkCompanynameAvailability() throws Exception {
         if (new ClientBusiness().clientExists(client.getCompanyName())) {
             throw new Exception("meaninfulerrormessage");
@@ -97,10 +131,17 @@ public class ManageClientBean implements Serializable {
         return "";
     }
 
+    /**
+     *
+     */
     public void setActiveStatus() {
         ClientBusiness cb = new ClientBusiness();
         for (Client client : clients) {
             cb.saveClient(client);
         }
+    }
+
+    public String toOverview() {
+        return "overviewClient";
     }
 }
